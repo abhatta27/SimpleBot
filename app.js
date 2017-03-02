0f8eaaa781fc64aadf53e6cde72236bd242600f7
Abhishek Bhattacharya <abhatt@sapient.com>
2017-03-02 13:56:30 +0530

    Created package.json and app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// Requirements
+var restify = require('restify');
+var builder = require("botbuilder");
+
+// Setup restify server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, 
+    function()
+    {
+        console.log('%s listening to %s', server.name, server.url);
+    }
+);
+
+// Create chat bot
+var connector = new builder.ChatConnector({ appId: 'YourAppId', appPassword: 'YourAppPassword' });
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function(session)
+{
+    session.send('hello world');
+});
