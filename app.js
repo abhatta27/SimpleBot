69112dec39bcd0c6410dceaa335806d2aae43468
Abhishek Bhattacharya <abhatt@sapient.com>
2017-03-03 17:16:11 +0530

    added two functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Requirements
 var restify = require('restify');
-var builder = require("botbuilder");
+var builder = require('botbuilder');
 
 // Setup restify server
 var server = restify.createServer();
@@ -12,12 +12,18 @@ server.listen(process.env.PORT || 3000,
 );
 
 // Create chat bot
-var connector = new builder.ChatConnector({ appId: 'appid', appPassword: 'password' });
+var connector = new builder.ChatConnector({ appId:'092cb0ca-92a9-461a-9103-97ac45363ca9', appPassword: 'axZjyVtLPyzRYADBjtKFcDy' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
-bot.dialog('/', function(session)
-{
-    session.send('hello world');
-});
+bot.dialog('/', [
+    function(session)
+    {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    }
+     function (session, results) 
+     {
+        session.send('Hello %s!', results.response);
+    }
+]);
